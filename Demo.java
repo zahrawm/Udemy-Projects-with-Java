0ec7e9e12ebca2093d76f9af803609552101d96f
Fatimah Zahra Adam <adamfatima2557@gmail.com>
2026-04-02 22:55:05 +0000

    While loops and Nested while loops

---
+++ Demo.java
@@ -0,0 +1,17 @@
+public class Demo {
+    public static void main(String[] args) {
+       int i = 1;
+       while ( i <= 4) {
+        System.out.println(" Hi " + i);
+        int g = 3;
+        while (g >= 1) {
+            System.out.println(" Hello " + g);
+            g --;
+         i ++;
+        
+       }
+       i ++;
+         System.out.println("Bye");
+    }
+}
+}

b3d418f5605c6725379343aaca66bd30054b778c
Fatimah Zahra Adam <adamfatima2557@gmail.com>
2026-04-03 22:21:17 +0000

    while loop leasons

---
+++ For.java
@@ -0,0 +1,9 @@
+public class For {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+           System.out.println("  " + i);
+             for (int j  = 6; j <= 10 ;j ++) {
+                System.out.println("  " + i  + " 8 " +  "_"  + 9);
+        }
+    }
+}}
---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    
+
+    public static void main(String[] args) {
+        int i = 45;
+         do {
+            System.out.println("Hi " + i);
+            i++;
+         } while (i <= 40);
+         System.out.println("Bye");
+}
+}

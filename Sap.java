474451ff873ab4389681ef8983cce7fb4062e0b5
Fatimah Zahra Adam <adamfatima2557@gmail.com>
2026-04-09 17:13:54 +0000

    Arrays concept completed

---
+++ Sap.java
@@ -0,0 +1,16 @@
+public class Sap {
+    public static void main(String[] args) {
+        int nums[] = new int[5];
+        nums[0] = 10;
+        nums[1] = 20;
+        nums[2] = 30;
+        nums[3] = 40;
+        nums[4] = 50;
+         for ( int n : nums){
+            System.out.println(n);
+         }
+         
+
+} 
+
+}

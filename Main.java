8ac2390aa1382f3c64c90ce304d4ddde445331fd
Fatimah Zahra Adam <adamfatima2557@gmail.com>
2026-04-23 18:34:35 +0000

    Try and catch Exceptions

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num = sc.nextInt();
+        System.out.println("You entered: " + num);
+    }
+}

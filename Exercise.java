85d72bf453c627101daa29de17029b2b190832b8
Fatimah Zahra Adam <adamfatima2557@gmail.com>
2026-04-25 12:40:43 +0000

    Exercise on Java with threads

---
+++ Exercise.java
@@ -0,0 +1,32 @@
+// PrinterTask class
+class PrinterTask implements Runnable {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(Thread.currentThread().getName() + ": Running task " + i);
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+public class Exercise {
+    public static void main(String[] args) {
+        PrinterTask task = new PrinterTask();
+
+        Thread worker1 = new Thread(task, "Worker-1");
+        Thread worker2 = new Thread(task, "Worker-2");
+
+        worker1.start();
+        worker2.start();
+
+        try {
+            worker1.join();
+            worker2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}

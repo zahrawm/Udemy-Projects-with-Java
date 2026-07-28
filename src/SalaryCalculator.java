81af44a0a56bd1729ddf1c1a8c17679fca13c157
Fatimah Zahra Adam <adamfatima2557@gmail.com>
2026-07-28 17:43:58 +0000

    Create SalaryCalculator in Java

---
+++ src/SalaryCalculator.java
@@ -0,0 +1,20 @@
+public class SalaryCalculator {
+
+    private static final double BASE_SALARY = 1000.0;
+
+    public double salaryMultiplier(int daysSkipped) {
+        return daysSkipped >= 5 ? 0.85 : 1.0;
+    }
+
+    public int bonusMultiplier(int productsSold) {
+        return productsSold < 20 ? 10 : 13;
+    }
+
+    public double bonusForProductsSold(int productsSold) {
+        return productsSold * bonusMultiplier(productsSold);
+    }
+
+    public double finalSalary(int daysSkipped, int productsSold) {
+        return (BASE_SALARY * salaryMultiplier(daysSkipped)) + bonusForProductsSold(productsSold);
+    }
+}
